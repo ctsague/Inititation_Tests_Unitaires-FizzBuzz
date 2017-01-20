f00a8bd3e4e980f53985e6f098f2fce62aab56b7
tsague <tsagueymele@yahoo.fr>
2017-01-20 11:21:26 +0100

    test1

--- spec/FizzBuzz.spec.js
+++ spec/FizzBuzz.spec.js
@@ -1,9 +1,9 @@
-var FizzBuzzOf = require('../lib/FizzBuzz').FizzBuzzOf
+var FizzBuzzOf = require('../lib/fizzBuzz').FizzBuzzOf
 
 describe("FizzBuzzOf execution", function () {
 
     it("should return ChangeMe at the beginning with parameter 1", function () {
-        expect(FizzBuzzOf(1)).toBe("ChangeMe");
+        expect(FizzBuzzOf(1)).toBe("1");
     });
 
 });
